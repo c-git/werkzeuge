52f7f70c55bb7b61e7e520e6dd09e9516ad0e44b
One <43485962+c-git@users.noreply.github.com>
2023-12-16 00:40:56 -0500

    Add tests

--- crates/snake_case/src/lib.rs
+++ crates/snake_case/src/lib.rs
@@ -21,3 +21,25 @@ fn convert_to_snake_case(org_str: &str) -> String {
     let re = Regex::new("[^0-9a-zA-Z_ ]").unwrap();
     re.replace_all(org_str, "").to_case(Case::Snake)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case("Simple Case", "simple_case")]
+    #[case("With !@#$%^&*()_+/\\ Special *Chars", "with_special_chars")]
+    #[case(
+        "Task 2: Unanimously Legendary IDentifier (ULID) ",
+        "task_2_unanimously_legendary_i_dentifier_ulid"
+    )]
+    #[case(
+        "Task 2: Unanimously Legendary IDentifier [ULID] ",
+        "task_2_unanimously_legendary_i_dentifier_ulid"
+    )]
+    fn conversion(#[case] input: &str, #[case] expected: &str) {
+        let actual = convert_to_snake_case(input);
+        assert_eq!(actual, expected);
+    }
+}
