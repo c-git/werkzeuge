fd1273ce68846821bcf84504fd37751c522c88cf
One <43485962+c-git@users.noreply.github.com>
2024-10-10 22:58:27 -0400

    feat: code to extract the speaker notes

--- app_script/speaker_notes_extractor/Code.js
+++ app_script/speaker_notes_extractor/Code.js
@@ -1,3 +1,12 @@
-function myFunction() {
-  
+function speakerNotes() {
+    const presentation = SlidesApp.getActivePresentation();
+    const slides = presentation.getSlides();
+    let allNotes = "";
+    slides.forEach((slide,index)=>{
+        const note = slide.getNotesPage().getSpeakerNotesShape().getText().asString();
+        allNotes += `#${index+1}\n`;
+        allNotes += note;
+    });
+    const doc = DocumentApp.openById("1xF9mFcSr-e5-DMOo2NPWbU2QzZugeSZ3kNAxQGgSIMQ");
+    doc.getBody().setText(allNotes);
 }
