d172d1e8cb44a7359a9fd5cd452df384069a4089
One <43485962+c-git@users.noreply.github.com>
2024-10-10 23:18:38 -0400

    feat: add button to run script

--- app_script/speaker_notes_extractor/Code.js
+++ app_script/speaker_notes_extractor/Code.js
@@ -1,4 +1,4 @@
-function speakerNotes() {
+function exportSpeakerNotes() {
     const presentation = SlidesApp.getActivePresentation();
     const doc = DocumentApp.openById("1xF9mFcSr-e5-DMOo2NPWbU2QzZugeSZ3kNAxQGgSIMQ");
     const doc_body = doc.getBody();
@@ -9,4 +9,10 @@ function speakerNotes() {
         doc_body.appendParagraph(`#${index+1}\n${note}`);
         doc_body.appendPageBreak();
     });
+}
+
+function onOpen() {
+    SlidesApp.getUi().createMenu('Custom')
+        .addItem('Export to GDoc', 'exportSpeakerNotes')
+        .addToUi();
 }
