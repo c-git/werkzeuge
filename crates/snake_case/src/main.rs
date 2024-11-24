62bf043e6b76a18735c092053f550ec50d569e2f
One <43485962+c-git@users.noreply.github.com>
2024-11-24 08:57:14 -0500

    refactor: switch to tracing

--- crates/snake_case/src/main.rs
+++ crates/snake_case/src/main.rs
@@ -1,9 +1,22 @@
 use clap::Parser;
-use log::debug;
 use snake_case::{self, run, Cli};
+use tracing::debug;
+use tracing_subscriber::{fmt, prelude::*, EnvFilter};
+
 fn main() -> anyhow::Result<()> {
     let cli: Cli = Cli::parse();
-    // TODO 1: Add tracing to replace log
+
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+            EnvFilter::new(if cfg!(debug_assertions) {
+                "info"
+            } else {
+                "warn"
+            })
+        }))
+        .init();
+
     debug!("Cli: {cli:#?}");
     let converted_text = run(&cli);
     println!("{converted_text}");
