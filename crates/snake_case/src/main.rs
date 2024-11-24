898456d2d86e0931a09c0d0fbd81c14f5f28d02e
One <43485962+c-git@users.noreply.github.com>
2024-11-24 08:15:33 -0500

    refactor: remove custom log crate
    Going to switch over to tracing and se environment variables for  level

--- crates/snake_case/src/main.rs
+++ crates/snake_case/src/main.rs
@@ -1,10 +1,9 @@
 use clap::Parser;
 use log::debug;
-use one_log::init_logging;
 use snake_case::{self, run, Cli};
 fn main() -> anyhow::Result<()> {
     let cli: Cli = Cli::parse();
-    init_logging(cli.log_level.into())?;
+    // TODO 1: Add tracing to replace log
     debug!("Cli: {cli:#?}");
     let converted_text = run(&cli);
     println!("{converted_text}");
